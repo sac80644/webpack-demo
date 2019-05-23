7a52d68256c685f7773ca46995b277a4fae85571
sac80644 <jason@gallowaytech.com>
2019-05-23 14:59:24 -0500

    css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,20 @@ const plugins = [
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+
     },
     plugins: plugins
 };
